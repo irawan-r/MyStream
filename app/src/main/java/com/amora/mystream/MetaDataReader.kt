e32306c990fa71635ccd39e08d2d9ea5842dff79
roy <irawanr567@gmail.com>
2023-12-19 12:40:34 +0700

    Initial commit

---
+++ app/src/main/java/com/amora/mystream/MetaDataReader.kt
@@ -0,0 +1,35 @@
+package com.amora.mystream
+
+import android.app.Application
+import android.net.Uri
+import android.provider.MediaStore
+
+data class MetaData(
+	val fileName: String
+)
+
+interface MetaDataReader {
+	fun getMetaDataFromUri(contentUri: Uri): MetaData?
+}
+
+class MetaDataReaderImpl(private val app: Application) : MetaDataReader {
+	override fun getMetaDataFromUri(contentUri: Uri): MetaData? {
+		val fileName = app.contentResolver
+			.query(
+				contentUri,
+				arrayOf(MediaStore.Video.VideoColumns.DISPLAY_NAME),
+				null,
+				null,
+				null
+			)
+			?.use { cursor ->
+				val index = cursor.getColumnIndex(MediaStore.Video.VideoColumns.DISPLAY_NAME)
+				cursor.moveToFirst()
+				cursor.getString(index)
+			}
+		return fileName?.let { fullFileName ->
+			MetaData(fileName = Uri.parse(fullFileName).lastPathSegment ?: return null)
+		}
+	}
+
+}
